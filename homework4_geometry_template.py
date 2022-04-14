da407a2f983b3a2f68364b156e471d37d43c7783
brian <briancfay1@gmail.com>
2022-04-14 13:43:19 -0400

    Added code to save and load weights and bases

--- homework4_geometry_template.py
+++ homework4_geometry_template.py
@@ -3,14 +3,29 @@ import matplotlib.pyplot as plt
 
 X = np.load("hw4_X.npy")
 y = np.load("hw4_y.npy")
-n = X.shape[1]//2
+n = X.shape[1] // 2
 
 x = np.arange(-8, +8, 0.01)
-plt.scatter(X[0,0:n], X[1,0:n])
-plt.scatter(X[0,n:], X[1,n:])
+plt.scatter(X[0, 0:n], X[1, 0:n])
+plt.scatter(X[0, n:], X[1, n:])
+
+graph_data = open('HW3_part2.csv', 'r').read()
+lines = graph_data.split('\n')
+ws = []
+bs = []
+
+for line in lines[1:]:
+    if len(line) > 1:
+        # batch_size, training_acc, testing_acc, training_time = line.split(',')
+        data = line.split(',')
+        ws.append(float(data[0]))
+        bs.append(float(data[1]))
+
+print("Weights:", ws, "Bases:", bs)
 
 # Plot some arbitrary parallel lines (*not* separating hyperplanes) just for an example
-plt.plot(x, x*-1.9+3, 'k-')
-plt.plot(x, x*-1.9+3+1, 'k--')
-plt.plot(x, x*-1.9+3-1, 'k:')
+#TODO make this based on ws and bs
+plt.plot(x, x * -1.9 + 3, 'k-')
+plt.plot(x, x * -1.9 + 3 + 1, 'k--')
+plt.plot(x, x * -1.9 + 3 - 1, 'k:')
 plt.show()
--- homework4_template.py
+++ homework4_template.py
@@ -1,16 +1,18 @@
+from turtle import pd
+
 from cvxopt import solvers, matrix
 import numpy as np
 import sklearn.svm
 
 
 class SVM4342:
-    def __init__ (self):
+    def __init__(self):
         pass
 
     # Expects each *row* to be an m-dimensional row vector. X should
     # contain n rows, where n is the number of examples.
     # y should correspondingly be an n-vector of labels (-1 or +1).
-    def fit (self, X, y):
+    def fit(self, X, y):
         print(X.shape)
         print(y.shape)
 
@@ -22,14 +24,12 @@ class SVM4342:
 
         # TODO change these -- they should be np.arrays representing matrices or vectors
 
-
-        G=np.zeros((sample_num,bias_data_len))
+        G = np.zeros((sample_num, bias_data_len))
         for i in range(bias_data_len):
-            print(X[:,i])
-            print(np.multiply(X[:,i],-y))
-            G[:,i]=np.multiply(X[:,i],-y)#makeynegativebysubtractingfromG
+            print(X[:, i])
+            print(np.multiply(X[:, i], -y))
+            G[:, i] = np.multiply(X[:, i], -y)  # makeynegativebysubtractingfromG
         # G = G.T
-
 
         h = np.ones(sample_num) * -1
         P = np.identity(bias_data_len)
@@ -43,7 +43,7 @@ class SVM4342:
         # that you flatten() the w you retrieve from the solution vector so that
         # it becomes a 1-D np.array.
         flattened_soln = np.array(sol['x']).flatten()
-        
+
         self.w = flattened_soln[:-1]  # TODO change this
         self.b = flattened_soln[-1]  # TODO change this
         zero = 0
@@ -54,17 +54,18 @@ class SVM4342:
         return X
 
     # Given a 2-D matrix of examples X, output a vector of predicted class labels
-    def predict (self, x):
+    def predict(self, x):
         samplenum, data_len_withbias = x.shape
         weights = np.atleast_2d(np.hstack((self.w, self.b))).T
         x = self.data4wewights(x)
         predictions = np.rint(np.dot(x, weights))
         return predictions  # TODO fix
 
-def test1 ():
+
+def test1():
     # Set up toy problem
-    X = np.array([ [1,1], [2,1], [1,2], [2,3], [1,4], [2,4] ])
-    y = np.array([-1,-1,-1,1,1,1])
+    X = np.array([[1, 1], [2, 1], [1, 2], [2, 3], [1, 4], [2, 4]])
+    y = np.array([-1, -1, -1, 1, 1, 1])
 
     # Train your model
     svm4342 = SVM4342()
@@ -84,15 +85,16 @@ def test1 ():
     acc = np.mean(our_pred == lib_pred)
     print("Acc={}".format(acc))
 
-def test2 (seed):
+
+def test2(seed):
     np.random.seed(seed)
 
     # Generate random data
-    X = np.random.rand(20,3)
+    X = np.random.rand(20, 3)
     # Generate random labels based on a random "ground-truth" hyperplane
     while True:
         w = np.random.rand(3)
-        y = 2*(X.dot(w) > 0.5) - 1
+        y = 2 * (X.dot(w) > 0.5) - 1
         # Keep generating ground-truth hyperplanes until we find one
         # that results in 2 classes
         if len(np.unique(y)) > 1:
@@ -104,7 +106,8 @@ def test2 (seed):
     # Compare with sklearn
     svm = sklearn.svm.SVC(kernel='linear', C=1e15)  # 1e15 -- approximate hard margin
     svm.fit(X, y)
-    print("svm coef shape = "); print(svm.coef_.flatten().shape)
+    print("svm coef shape = ");
+    print(svm.coef_.flatten().shape)
     diff = np.linalg.norm(svm.coef_.flatten() - svm4342.w) + np.abs(svm.intercept_ - svm4342.b)
     print(diff)
 
@@ -114,7 +117,16 @@ def test2 (seed):
     if acc == 1 and diff < 1e-1:
         print("Passed")
 
-if __name__ == "__main__": 
+#w and b must be arrays
+def vector2csv(w, b):
+    file_path = "HW4_Vectors.csv"
+    my_df = {'Weights': w,
+             'Bases': b}
+    df = pd.DataFrame(my_df)
+    df.to_csv(file_path, header=False)
+
+
+if __name__ == "__main__":
     test1()
-    #for seed in range(5):
-    #    test2(seed)
+    for seed in range(5):
+        test2(seed)
